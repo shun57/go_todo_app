c8bfb6ca1b48dc2fc80aea843ad4e40aaa43e680
shun57 <tuktuk.032@gmail.com>
2023-01-14 22:28:28 +0900

    CHAPTER19：HTTPハンドラーの実装を分解する

--- go.mod
+++ go.mod
@@ -19,5 +19,9 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/matryer/moq v0.3.0 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
--- handler/add_task.go
+++ handler/add_task.go
@@ -5,14 +5,11 @@ import (
 	"net/http"
 
 	"github.com/go-playground/validator"
-	"github.com/jmoiron/sqlx"
 	"github.com/shun57/go_todo_app/entity"
-	"github.com/shun57/go_todo_app/store"
 )
 
 type AddTask struct {
-	DB        *sqlx.DB
-	Repo      *store.Repository
+	Service   AddTaskService
 	Validator *validator.Validate
 }
 
@@ -38,7 +35,7 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		Title:  b.Title,
 		Status: entity.TaskStatusTodo,
 	}
-	err := at.Repo.AddTask(ctx, at.DB, t)
+	t, err := at.Service.AddTask(ctx, b.Title)
 	if err != nil {
 		RespondJSON(ctx, w, &ErrorResponse{
 			Message: err.Error(),
